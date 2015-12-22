1bb6b555aa10f5ca1c71fde516de2959bcce79fc
timkersch <tim.kersch@gmail.com>
2015-12-23 00:24:41 +0100

    Updated models

--- text_analysis/migrations/0002_identifierword.py
+++ text_analysis/migrations/0002_identifierword.py
@@ -2,20 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import text_analysis.custom_fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('text_analysis', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='SpamData',
+            name='IdentifierWord',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('spam_data', text_analysis.custom_fields.SerializedDataField()),
+                ('word', models.CharField(max_length=30)),
             ],
             options={
             },
