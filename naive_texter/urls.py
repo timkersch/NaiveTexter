1e125c6c9b17dbc78655b0d01a0256abb67d1200
timkersch <tim.kersch@gmail.com>
2015-12-25 23:23:00 +0100

    Updated routing

--- naive_texter/urls.py
+++ naive_texter/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       url(r'^spam_classifier/', include('spam_classifier.urls')),
+                       url(r'', include('spam_classifier.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
