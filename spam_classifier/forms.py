9b3b4324a98d085e40a63fd3f60456375df94c1a
timkersch <tim.kersch@gmail.com>
2016-01-13 23:51:25 +0100

    Some cleanup

--- spam_classifier/forms.py
+++ spam_classifier/forms.py
@@ -9,7 +9,7 @@ CHOICES = (
 
 class TextForm(forms.Form):
 	text = forms.CharField(label="", help_text="", widget=forms.Textarea(attrs={'placeholder':'Text to process...', 'cols':10}))
-	choice = forms.ChoiceField(choices=CHOICES)
+	choice = forms.ChoiceField(label="Analysis method:",choices=CHOICES)
 
 class ImmutableTextForm(forms.Form):
 	text = forms.CharField(label="Processed text", help_text="", widget=forms.Textarea(attrs={'cols':10, 'disabled':'true'}))
